7315cb8c8eaab06c01ba70fde53f21fdac5bd049
Vanessa González <carol.gonzlez05@gmail.com>
2026-06-06 23:10:26 -0500

    Delete js/lang.js

--- js/lang.js
+++
@@ -1,9 +0,0 @@
-let currentLang = 'es';
-
-function toggleLang() {
-  currentLang = currentLang === 'es' ? 'en' : 'es';
-  document.querySelectorAll('[data-es]').forEach(el => {
-    el.textContent = el.dataset[currentLang];
-  });
-  document.querySelector('.lang-btn').textContent = currentLang === 'es' ? 'EN / ES' : 'ES / EN';
-}
